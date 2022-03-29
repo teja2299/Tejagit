820c77999245a23493a9f6a3796150d598a8245c
teja2299 <saitejaganjikunta@gmai.com>
2022-03-29 12:43:21 +0530

    Instruct devops engineer

--- company.sh
+++ company.sh
@@ -1,4 +1,6 @@
 Hello everyone 
 Here we are going to start GIT from today
 The first tool in devops course
+The changes may happen by any developer in the team
+so, devops engineer is responsible to save or merge the file correctly
 
